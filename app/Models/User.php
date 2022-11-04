7a727360c7c46a89672d18491ee1108d7c6f3d8b
maki <lindon446@gmail.com>
2022-11-04 13:06:34 +0800

    邮箱验证功能

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticate;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticate implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
